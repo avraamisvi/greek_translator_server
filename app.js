1bca535815293475f33b6525c286b5fa5bd4f425
abraaocarmo <asouza@wine.com>
2017-11-16 21:59:41 -0200

    ajustes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var cors = require('cors')
 
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
